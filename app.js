01f96259ae6e9846bff40a0763db4d74ddf56510
Andrew Murray <darbyguitars@gmail.com>
2019-01-03 14:58:16 -0600

    going to change branch for a lecture item

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log('it is here')
+
+const moveItem = function(event) {
+	console.log(event.target.parentElement)
+	$('#done-list').append(event.target.parentElement)
+	$(event.target).off()
+}
+
+$('#add-task-button').click(function() {
+	//this declares task
+	let task = $('#add-task-input').val()
+	let $button = $('<button>Done</button>').click(moveItem)
+	//put conditional here
+	if (task === '') {
+		alert('Task cannot be blank')
+		// every thing else
+	} else {
+		let $li = $('<li>' + task + '</li>')
+		$('#to-do-list').append($li)
+		$li.append($button)
+		$('#add-task-input').val('')
+	}
+})
